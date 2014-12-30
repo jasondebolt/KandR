a41db9bf6a44fd3bc9127d3e8d1e65c17f99c61f
jasondebolt <jasondebolt@gmail.com>
2014-12-30 10:31:03 -0800

    1-19.c

--- chapter1/1-19.c
+++ chapter1/1-19.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <assert.h>
+#define MAX_STR_LEN 1000
 
-void reverse(char s[]);
+int getln(char s[], int limit);
 int strln(char s[]);
+void reverse(char s[]);
 void swap(char *first, char *second);
 
+
+void fillArray(char s[], int limit) {
+  while (--limit >= 0) {
+    s[limit] = '\0';
+  }
+}
+
 int main() {
-  char s[] = "jason was here";
-  reverse(s);
-  printf("%s\n", s);
+  char s[MAX_STR_LEN];
+  int i;
+  fillArray(s, MAX_STR_LEN);
+  while ((i = getln(s, MAX_STR_LEN)) >= 0) {
+    reverse(s);
+    printf("%s\n", s);
+  }
   return 0;
+}
+
+// Read line into a string literal (character array)
+int getln(char s[], int limit) {
+  char c;
+  int cnt;
+  for (cnt = 0; (c = getchar()) != '\n' && cnt <= limit; ++cnt) {
+    s[cnt] = c;
+  }
+  s[cnt] = '\0';
+  return cnt;
 }
 
 void reverse(char s[]) {
