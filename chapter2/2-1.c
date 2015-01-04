b1a370e6e7a07ab58383fac9ec130a15bba55f56
jasondebolt <jasondebolt@gmail.com>
2015-01-04 15:43:14 -0800

    adding 2-1.c

---
+++ chapter2/2-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <limits.h>
+
+
+int main() {
+	
+	printf("CHAR_BIT: %d\n", CHAR_BIT);
+	printf("CHAR_MIN: %d\n", CHAR_MIN);
+	printf("CHAR_MAX: %d\n", CHAR_MAX);
+	printf("SCHAR_MIN: %d\n", SCHAR_MIN);
+	printf("SCHAR_MAX: %d\n", SCHAR_MAX);
+	printf("UCHAR_MAX: %d\n", UCHAR_MAX);
+	printf("SHRT_MIN: %d\n", SHRT_MIN);
+	printf("SHRT_MAX: %d\n", SHRT_MAX);
+	printf("USHRT_MAX: %d\n", USHRT_MAX);
+	printf("INT_MIN: %d\n", INT_MIN);
+	printf("INT_MAX: %d\n", INT_MAX);
+	printf("UINT_MAX: %d\n", UINT_MAX);
+	printf("LONG_MIN: %ld\n", LONG_MIN);
+	printf("LONG_MAX: %ld\n", LONG_MAX);
+	printf("ULONG_MAX: %ld\n", ULONG_MAX);
+	
+	
+	return 0;
+}
