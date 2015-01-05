fba974ea6f36fbe32afdf64c727c0adbc50ffac1
jasondebolt <jasondebolt@gmail.com>
2015-01-04 16:52:59 -0800

    atoi.c

---
+++ chapter2/atoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int atoi(char s[]) {
+  int i, n;
+
+  n = 0;
+  for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i) {
+    n = 10 * n + (s[i] - '0');
+  }
+  return n;
+}
+
+int main() {
+  
+  printf("%d\n", atoi("123"));
+  return 0;
+}
