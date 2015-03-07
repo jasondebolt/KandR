987313966dd11d2fbb8a82b5657aefbc0e731126
Jason Debolt <jbenjamin@jbenjamin-macbookpro2.roam.corp.google.com>
2015-03-07 14:19:03 -0800

    Implement the UNIX program sort (version 6) - multi field sorting

--- unix_sort_my_version.c
+++ unix_sort_my_version.c
@@ -3,9 +3,14 @@
 #include <assert.h>
 #include <string.h>
 #define MAX_LINE_LEN 1024
+#define MAX_DELTA 0.0000000001
 #define MAX_LINES 10000
+#define MAX_FIELD_NUM 1024
 #define MAX_BUF_LEN MAX_LINE_LEN * MAX_LINES
 #define MAX_LINE_STORE MAX_LINES * 100
+#define DEBUG 0
+
+typedef int (*COMP)(void *, void *, int, int, int);
 
 static char buf[MAX_BUF_LEN];
 static char *bufp = buf;
@@ -46,10 +51,12 @@ int numComp(char *a, char *b, int reverse, int folding, int directory) {
     return (reverse) ? -1 : 1;
   }
   return 0;
+
 }
 
-char * skipUntilLetterNumberBlank(char *c) {
-  while (!isLetterNumberBlank(*c)) {
+char * skipUntilLetterNumberBlankOrNull(char *c) {
+  // We check for null to ensure we do not go beyond the end of the string.
+  while (!isLetterNumberBlank(*c) && *c != '\0') {
     ++c;
   }
   return c;
@@ -57,16 +64,16 @@ char * skipUntilLetterNumberBlank(char *c) {
 
 int strCompFold(char *a, char *b, int reverse, int folding, int directory) {
   if (directory) {
-    a = skipUntilLetterNumberBlank(a);
-    b = skipUntilLetterNumberBlank(b);
+    a = skipUntilLetterNumberBlankOrNull(a);
+    b = skipUntilLetterNumberBlankOrNull(b);
   }
   if (folding) {
     while (toLower(*a) == toLower(*b) && *a != '\0') {
       a++;
       b++;
       if (directory) {
-        a = skipUntilLetterNumberBlank(a);
-        b = skipUntilLetterNumberBlank(b);
+        a = skipUntilLetterNumberBlankOrNull(a);
+        b = skipUntilLetterNumberBlankOrNull(b);
       }
     }
     return (reverse) ? toLower(*b) - toLower(*a) : toLower(*a) - toLower(*b);
@@ -75,17 +82,227 @@ int strCompFold(char *a, char *b, int reverse, int folding, int directory) {
       a++;
       b++;
       if (directory) {
-        a = skipUntilLetterNumberBlank(a);
-        b = skipUntilLetterNumberBlank(b);
+        a = skipUntilLetterNumberBlankOrNull(a);
+        b = skipUntilLetterNumberBlankOrNull(b);
       }
     }
     return (reverse) ? *b - *a : *a - *b;
   }
 }
 
-
 int strComp(char *a, char *b, int reverse, int folding, int directory) {
   return strCompFold(a, b, reverse, folding, directory);
+}
+
+
+int strFieldComp(char *a, char *b, int folding, int reverse, int directory) {
+  if (directory) {
+    a = skipUntilLetterNumberBlankOrNull(a);
+    b = skipUntilLetterNumberBlankOrNull(b);
+  }
+  if (folding) {
+    while (toLower(*a) == toLower(*b) && *a != '\0' && *a != ' ') {
+      a++;
+      b++;
+      if (directory) {
+        a = skipUntilLetterNumberBlankOrNull(a);
+        b = skipUntilLetterNumberBlankOrNull(b);
+      }
+    }
+    return (reverse) ? toLower(*b) - toLower(*a) : toLower(*a) - toLower(*b);
+  } else {
+    while (*a == *b && *a != '\0' && *a != ' ') {
+      a++;
+      b++;
+      if (directory) {
+        a = skipUntilLetterNumberBlankOrNull(a);
+        b = skipUntilLetterNumberBlankOrNull(b);
+      }
+    }
+    return (reverse) ? *b - *a : *a - *b;
+  }
+}
+
+int numFieldComp(char *a, char *b, int folding, int reverse, int directory) {
+  // Skip whitespace.
+  while (*a == ' ' || *a == '\t')
+    a++;
+  while (*b == ' ' || *a == '\t')
+    b++;
+  int a_negative, b_negative;
+  a_negative = b_negative = 0;
+  if (*a == '-') {
+    a_negative = 1;
+    a++;
+  }
+  if (*b == '-') {
+    b_negative = 1;
+    b++;
+  }
+  if (directory) {
+    // Implement sqeeze.
+    char *a_start = a;
+    char *a_numbers_only = a;
+    while (*a != '\0' && *a != ' ') {
+      if ((*a >= '0' && *a <= '9') || *a == '.') {
+        *a_numbers_only++ = *a;
+      }
+      a++;
+    }
+    *a_numbers_only = '\0';
+    a = a_start;
+    // Implement sqeeze.
+    char *b_start = b;
+    char *b_numbers_only = b;
+    while (*b != '\0' && *b != ' ') {
+      if ((*b >= '0' && *b <= '9') || *b == '.') {
+        *b_numbers_only++ = *b;
+      }
+      b++;
+    }
+    *b_numbers_only = '\0';
+    b = b_start;
+  }
+  double result = atof(a) - atof(b);
+  double a_result = (result < 0) ? -1 * result : result;
+  if (a_result < MAX_DELTA || result == 0) {
+    return 0;
+  } else if (result < 0) {
+    return (reverse) ? 1 : -1;
+  } else {
+    return (reverse) ? -1 : 1;
+  }
+}
+
+// field_num is 0 indexed.
+int compAnyField(const char *a, const char *b, int field_num, int numeric,
+    int folding, int reverse, int directory) {
+
+  COMP comp = (COMP) ((numeric) ? numFieldComp : strFieldComp);
+  // skip intial whitespace.
+  while (*a == ' ')
+    a++;
+  while (*b == ' ')
+    b++;
+
+  while (--field_num >= 0) {
+    while (*a != ' ' && *a != '\0')
+      a++;
+    while (*b != ' ' && *b != '\0')
+      b++;
+
+    // Handle case of either string not having enough fields before field_num.
+    if (*a == '\0' || *b == '\0')
+      return *a - *b; // A string without enough fields will be place before.
+
+    // skip white space between fields.
+    while (*a == ' ' && *a != '\0')
+      ++a;
+    while (*b == ' ' && *b != '\0')
+      ++b;
+  }
+  if (DEBUG)
+    printf("%s Comparing a: %s, to b: %s\n", (numeric) ? "Num" : "Str", a, b);
+
+  // Finally, we compare.
+  return comp((void *)a, (void *)b, folding, reverse, directory);
+}
+
+// Sets flags based on an argv field string like "-5nr".
+void setFlagsFromArgv(
+    char *argv2, int *field_num, int *numeric, int *folding, int *reverse,
+    int *directory) {
+    if (*argv2 != '-')
+      fprintf(stderr, "Field argument must start with a dash.");
+
+    // skip dash.
+    argv2++;
+    if (!(*argv2 >= '0' || *argv2 <= '9'))
+      fprintf(stderr, "field number is required.");
+
+    char num[MAX_FIELD_NUM];
+    char *num_p = num;
+    while (*argv2 >= '0' && *argv2 <= '9') {
+      *num_p++ = *argv2++;
+    }
+    *num_p = '\0';
+    *field_num = (int) atof(num);
+
+    while (*argv2) {
+      switch (*argv2) {
+        case 'n':
+          *numeric = 1;
+          break;
+        case 'f':
+          *folding = 1;
+          break;
+        case 'r':
+          *reverse = 1;
+          break;
+        case 'd':
+          *directory= 1;
+          break;
+      }
+      argv2++;
+    }
+}
+
+// *argv may be something like "./a.out -F -0rf -1nd -4rn
+int badAssCompare(const char *a, const char *b, int argc, char *argv[]) {
+  int field_num, numeric, folding, reverse, directory;
+  int comp_result;
+  while (--argc > 0) {
+    field_num = numeric = folding = reverse = directory = 0;
+    char *argv2 = *++argv;
+    // Skip "-F" field.
+    if (strcmp(argv2, "-F") == 0) {
+      continue;
+    }
+    setFlagsFromArgv(argv2, &field_num, &numeric, &folding, &reverse, &directory);
+    if ((comp_result = compAnyField(
+        a, b, field_num, numeric, folding, reverse, directory)) != 0) {
+      if (DEBUG)
+        printf("comp_result is %d, returning.\n", comp_result);
+      return comp_result;
+    }
+    if (DEBUG)
+      printf("comp_result is %d, continuing.\n", comp_result);
+  }
+  return 0;
+}
+
+int strCompFirstField(const char *a, const char *b, int folding,
+    int reverse, int directory) {
+  while (*a == *b && *a != '\0' && *a != ' ') {
+    ++a;
+    ++b;
+  }
+  return *a - *b;
+}
+
+
+int strCompSecondField(const char *a, const char *b, int folding,
+    int reverse, int directory) {
+  // skip intial whitespace.
+  while (*a == ' ')
+    a++;
+  while (*b == ' ')
+    b++;
+
+  // skip first field.
+  while (*a != ' ' && *a != '\0')
+    a++;
+  while (*b != ' ' && *b != '\0')
+    b++;
+
+  // Handle case of either string not having a second field.
+  if (*a == '\0' || *b == '\0')
+    return *a - *b;
+
+  ++a, ++b; // skip white space.
+  while (*a == *b && *a != '\0' && *a != ' ')
+    ++a, ++b;
+  return *a - *b;
 }
 
 char *alloc2(size_t len) {
@@ -185,6 +402,21 @@ void QuickSort(void *vals[], int low, int high,
   SwapGeneric((void **) vals, k, low);
   QuickSort((void **) vals, low, k - 1, comp, reverse, folding, directory);
   QuickSort((void **) vals, k + 1, high, comp, reverse, folding, directory);
+}
+
+void QuickSortMulti(void *vals[], int low, int high, int argc, char *argv[]) {
+  if (high <= low)
+    return;
+  int mid = (low + high) / 2;
+  int k, i;
+  SwapGeneric((void **) vals, low, mid);
+  for (k = low, i = low + 1; i <= high; ++i) {
+    if (badAssCompare(vals[i], vals[low], argc, argv) < 0)
+      SwapGeneric((void **) vals, ++k, i);
+  }
+  SwapGeneric((void **) vals, k, low);
+  QuickSortMulti((void **) vals, low, k - 1, argc, argv);
+  QuickSortMulti((void **) vals, k + 1, high, argc, argv);
 }
 
 void QuickSortStrings(char *vals[], int low, int high) {
@@ -362,11 +594,173 @@ void testAtof() {
   printf("Atof tested.\n");
 }
 
+void testStrCompFirstField() {
+  char first[MAX_LINE_LEN] = "ccc ddd";
+  char second[MAX_LINE_LEN] = "aaa bbb";
+  assert(strCompFirstField(first, second, 0, 0, 0) > 0);
+  assert(strCompFirstField(second, first, 0, 0, 0) < 0);
+  assert(strCompFirstField(first, first, 0, 0, 0) == 0);
+  assert(strCompFirstField(second, second, 0, 0, 0) == 0);
+  printf("strCompFirstField tested.\n");
+}
+
+void testStrCompSecondField() {
+  char first[MAX_LINE_LEN] = "ccc ddd";
+  char second[MAX_LINE_LEN] = "aaa bbb";
+  char third[MAX_LINE_LEN] = "aaa ddd";
+  char fourth[MAX_LINE_LEN] = "aaa abcd";
+  char fifth[MAX_LINE_LEN] = "zoo aaa";
+  char sixth[MAX_LINE_LEN] = "zoo";
+  char seventh[MAX_LINE_LEN] = "     one ddd";
+  assert(strCompSecondField(first, third, 0, 0, 0) == 0);
+  assert(strCompSecondField(first, second, 0, 0, 0) > 0);
+  assert(strCompSecondField(second, third, 0, 0, 0) < 0);
+  assert(strCompSecondField(fourth, fifth, 0, 0, 0) > 0);
+  assert(strCompSecondField(fifth, sixth, 0, 0, 0) > 0);
+  assert(strCompSecondField(sixth, fifth, 0, 0, 0) < 0);
+  assert(strCompSecondField(first, seventh, 0, 0, 0) == 0);
+  printf("strCompSecondField tested.\n");
+}
+
+void testCompAnyFieldWithString() {
+  char zeroth[MAX_LINE_LEN] = "cc";
+  char first[MAX_LINE_LEN] = "ccc ddd ggg hhh nnn";
+  char second[MAX_LINE_LEN] = "aaa bbb jjj kkk ccc";
+  assert(compAnyField(zeroth, first, 0, 0, 0, 0, 0) < 0);
+  assert(compAnyField(zeroth, first, 4, 0, 0, 0, 0) < 0);
+  assert(compAnyField(first, second, 0, 0, 0, 0, 0) > 0);
+  assert(compAnyField(first, second, 1, 0, 0, 0, 0) > 0);
+  assert(compAnyField(first, second, 2, 0, 0, 0, 0) < 0);
+  assert(compAnyField(first, second, 3, 0, 0, 0, 0) < 0);
+  assert(compAnyField(first, second, 4, 0, 0, 0, 0) > 0);
+  printf("compAnyField with string tested.\n");
+}
+
+void testCompAnyFieldWithNumber() {
+  char first[MAX_LINE_LEN] = "123.456 888.999";
+  char second[MAX_LINE_LEN] = "123.456 111.444";
+  char third[MAX_LINE_LEN] = "01";
+  char fourth[MAX_LINE_LEN] = "1925071251257125.94875987298752525247082623 14";
+  char fifth[MAX_LINE_LEN] = "1925071251257125.94875987298752525247082623 29.14";
+  char sixth[MAX_LINE_LEN] = "11";
+  char seventh[MAX_LINE_LEN] = "100";
+  assert(compAnyField(first, second, 0, 1, 0, 0, 0) == 0);
+  assert(compAnyField(first, second, 1, 1, 0, 0, 0) > 0);
+  assert(compAnyField(first, third,  0, 1, 0, 0, 0) > 0);
+  assert(compAnyField(fourth, fifth, 0, 1, 0, 0, 0) == 0);
+  assert(compAnyField(fourth, fifth, 1, 1, 0, 0, 0) < 0);
+  assert(compAnyField(sixth, seventh, 0, 1, 0, 0, 0) < 0);
+  printf("compAnyField with number tested.\n");
+}
+
+void testSetFlagsFromArgv() {
+  int field_num, numeric, folding, reverse, directory;
+
+  field_num = numeric = folding = reverse = directory = 0;
+  setFlagsFromArgv("-0rn", &field_num, &numeric, &folding, &reverse, &directory);
+  assert(field_num == 0 && numeric && !folding && reverse && !directory);
+
+  field_num = numeric = folding = reverse = directory = 0;
+  setFlagsFromArgv("-6rnfd", &field_num, &numeric, &folding, &reverse, &directory);
+  assert(field_num == 6 && numeric && folding && reverse && directory);
+
+  field_num = numeric = folding = reverse = directory = 0;
+  setFlagsFromArgv("-13d", &field_num, &numeric, &folding, &reverse, &directory);
+  assert(field_num == 13 && !numeric && !folding && !reverse && directory);
+
+  field_num = numeric = folding = reverse = directory = 0;
+  setFlagsFromArgv("-28rdf", &field_num, &numeric, &folding, &reverse, &directory);
+  assert(field_num == 28 && !numeric && folding && reverse && directory);
+
+  field_num = numeric = folding = reverse = directory = 0;
+  setFlagsFromArgv("-0", &field_num, &numeric, &folding, &reverse, &directory);
+  assert(field_num == 0 && !numeric && !folding && !reverse && !directory);
+
+  printf("setFlagsFromArgv tested.\n");
+}
+
+void testNumFieldComp() {
+  char first[MAX_LINE_LEN] = "333";
+  char second[MAX_LINE_LEN] = "/Ejasonblah2/22/2";
+  char third[MAX_LINE_LEN] = "  bbb333   ";
+  char fourth[MAX_LINE_LEN] = "    BBB/2/2/2/2 ";
+  assert(numFieldComp(first, second, 0, 0, 1) < 0);
+  assert(numFieldComp(third, fourth, 0, 0, 1) < 0);
+  printf("numFieldComp tested.\n");
+}
+
+void testBadAssCompare() {
+  char first[MAX_LINE_LEN] = "ccc 123";
+  char second[MAX_LINE_LEN] = "bbb 2/22";
+  char third[MAX_LINE_LEN] = "BBB 333";
+  char fourth[MAX_LINE_LEN] = "aaa 123";
+  char *argv[MAX_LINE_LEN] = {"./a.out", "-F", "-0rf", "-1nd"};
+  assert(badAssCompare(first, second, 4, argv) < 0);
+  assert(badAssCompare(second, third, 4, argv) < 0);
+  assert(badAssCompare(third, fourth, 4, argv) < 0);
+  // Sort first by second column numeric/directory, then by first
+  // colum string reverse folding.
+  // "ccc 123"       "ccc 123"
+  // "bbb 2/22" -->  "aaa 123"
+  // "BBB 333";      "bbb 2/22"
+  // "aaa 123";      "BBB 333"
+  char *argv2[MAX_LINE_LEN] = {"./a.out", "-F", "-1nd", "-0rf"};
+  assert(badAssCompare(first, fourth, 4, argv2) < 0);
+  assert(badAssCompare(fourth, second, 4, argv2) < 0);
+  assert(badAssCompare(second, third, 4, argv2) < 0);
+
+  char first_2[MAX_LINE_LEN] =  "a b c g e p/ ;t 23 c()";
+  char second_2[MAX_LINE_LEN] = "d a i g r (P T$ 23 &c[]";
+
+  char *argv4[MAX_LINE_LEN] = {"./a.out", "-F", "-0"};
+  assert(badAssCompare(first_2, second_2, 3, argv4) < 0);
+
+  char *argv5[MAX_LINE_LEN] = {"./a.out", "-F", "-1"};
+  assert(badAssCompare(second_2, first_2, 3, argv5) < 0);
+
+  char *argv6[MAX_LINE_LEN] = {"./a.out", "-F", "-0r"};
+  assert(badAssCompare(second_2, first_2, 3, argv6) < 0);
+
+  char *argv7[MAX_LINE_LEN] = {"./a.out", "-F", "-3"};
+  assert(badAssCompare(first_2, second_2, 3, argv7) == 0);
+
+  char *argv8[MAX_LINE_LEN] = {"./a.out", "-F", "-3", "-4"};
+  assert(badAssCompare(first_2, second_2, 4, argv8) < 0);
+
+  char *argv9[MAX_LINE_LEN] = {"./a.out", "-F", "-3", "-7"};
+  assert(badAssCompare(first_2, second_2, 4, argv9) == 0);
+
+  char *argv10[MAX_LINE_LEN] = {"./a.out", "-F", "-3", "-5df", "-6df", "-7"};
+  assert(badAssCompare(first_2, second_2, 6, argv10) == 0);
+
+  char *argv11[MAX_LINE_LEN] = {"./a.out", "-F", "-7", "-6df", "-5df", "-3"};
+  assert(badAssCompare(first_2, second_2, 6, argv11) == 0);
+
+  char *argv12[MAX_LINE_LEN] = {"./a.out", "-F", "-8d"};
+  assert(badAssCompare(first_2, second_2, 3, argv12) == 0);
+
+  char first_1[MAX_LINE_LEN] = "k t c() 4 /g";
+  char second_1[MAX_LINE_LEN] = "j l &c[] - ;G";
+  char *argv3[MAX_LINE_LEN] = {"./a.out", "-F", "-2d", "-4df"};
+  assert(badAssCompare(first_1, second_1, 4, argv3) == 0);
+
+  char first_3[MAX_LINE_LEN] =  "a    b     c   g   e   p/   ;t   23  c()";
+  char second_3[MAX_LINE_LEN] = "d a i g r (P T$ 23 &c[]";
+
+  char *argv38[MAX_LINE_LEN] = {"./a.out", "-F", "-0"};
+  assert(badAssCompare(first_3, second_3, 3, argv38) < 0);
+
+  char *argv39[MAX_LINE_LEN] = {"./a.out", "-F", "-1"};
+  assert(badAssCompare(second_3, first_3, 3, argv39) < 0);
+
+  printf("badAssCompare tested.\n");
+}
+
 
 // USAGE: ./a.out
-// ./a.out -nr
-// ./a.out -n -r
-int main(int argc, char *argv[]) {
+// ./a.out -nrrf
+// ./a.out -n -r -f -d
+void UnixSortTest(int argc, char *argv[]) {
   int (*comp)(void *, void *, int, int, int);
   comp = (int (*)(void *, void *, int, int, int)) strComp;
   // Handle numeric sort.
@@ -397,18 +791,82 @@ int main(int argc, char *argv[]) {
   }
   if (numeric)
     comp = (int (*)(void *, void *, int, int, int)) numComp;
-  //testAlloc();
+  testUnixSortGeneric(comp, reverse, folding, directory);
+  printf("Unix sort tested.\n");
+}
+
+void MultiFieldUnixSortTest(int argc, char *argv[]) {
+  char *g[MAX_LINES];
+  char linestore[MAX_LINE_STORE];
+  size_t len;
+  len = ReadLinesFaster(g, linestore, MAX_LINES);
+  QuickSortMulti((void **) g, 0, len - 1, argc, argv);
+  WriteLines(g, len);
+  printf("\nMulti fiel unix sort tested.\n");
+}
+
+void BaseTests() {
+  // Interactive tests...
   //testReadLine();
   //testReadLines();
   //testWriteLine();
   //testWriteLines();
-  //testQuickSortStrings();
-  //testSwapGeneric();
-  //testNumComp();
-  //testAtof();
-  //testQuickSort();
   //testUnixSort();
+  testAlloc();
+  testQuickSortStrings();
+  testSwapGeneric();
+  testNumComp();
+  testAtof();
+  testQuickSort();
   testStrCompFold();
   testToLower();
-  testUnixSortGeneric(comp, reverse, folding, directory);
+  testStrCompFirstField();
+  testStrCompSecondField();
+  testCompAnyFieldWithString();
+  testCompAnyFieldWithNumber();
+  testSetFlagsFromArgv();
+  testNumFieldComp();
+  testBadAssCompare();
+  printf("Base tests tested.\n");
+}
+
+/* USAGE:
+ * d: directory (ignore any chars that are not letters, numbers, or whitespace)
+ * f: fold: (ignore case)
+ * n: numeric: (sort numerically, convert string to double)
+ * r: reverse: (reverse sort)
+ *
+ *   For UnixSort:
+ *     ./a.out -nrrf
+ *     ./a.out -n -r -f -d
+ *
+ *   For MultiFieldUnixSort:
+ *     ./a.out -F -0nr  // First field numeric reverse sort.
+ *     ./a.out -F -1df  // Second field ignore non-alphanumeric/case chars sort.
+ *     ./a.out -F -7n -5df -3d -0rn // Sort by 8th field numeric, then by 6th
+ *                                  // field directory folding, then by 4th
+ *                                  // field directory, then by first field
+ *                                  // numeric reverse order.
+ */
+int main(int argc, char *argv[]) {
+  //BaseTests();
+  //UnixSortTest(argc, argv);
+  // TODO(jason): Handle delimeter types other than blank space(s).
+  // TODO(jason): When using comma as a delimeter, handle
+  //              double optional quote delimited fields by default (jason "De Bolt")
+  //              (See the fake_data_with_quotes.csv and
+  //               fake_data_with_optional_quotes.csv files) The last field in
+  //               latter file has optional quotes.
+  // TODO(jason): Handle sorting dates in mm/dd/yyyy forat (hard to do)
+  if (argc == 1 || strcmp(argv[1], "-F") != 0 ) {
+    printf("./a.out -F -0nr  // First field numeric reverse sort.\n");
+    printf("./a.out -F -1df  // Second field ignore non-alphanumeric/case chars sort.\n");
+    printf("./a.out -F -7n -5df -3d -0rn // Sort by 8th field numeric, then by 6th\n");
+    printf("                             // field directory folding, then by 4th\n");
+    printf("                             // field directory, then by first field\n");
+    printf("                                  // numeric reverse order.\n");
+    printf("cat {file} | ./a.out -F -0\n");
+  } else {
+    MultiFieldUnixSortTest(argc, argv);
+  }
 }
