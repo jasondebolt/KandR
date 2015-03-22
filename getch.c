d26150f05e33ad565a0383831c2d2bbf0d70af77
Jason Debolt <jbenjamin@jbenjamin-macbookpro2.roam.corp.google.com>
2015-03-22 14:25:48 -0700

    Implement getword (version 1)

--- getch.c
+++ getch.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX_LINE_LEN 1024
 
 #define MAX_BUFF_SIZE 1024
 int buffer[MAX_BUFF_SIZE];
@@ -29,12 +32,41 @@ void ungets(char s[]) {
     ungetch(s[--i]);
 }
 
+/* getword: get next word or character from input.
+ * Copied from page 136 of K&R.
+ * Fetches the next "word" from the input, where a word is either a string
+ * of letters and digits beginning with a letter, or a single non-whitespace
+ * character. The return value is the first character of the word, or EOF
+ * for end of file, or the character itself if it is not alphabetic.
+ */
+int getword(char *word, int lim) {
+  int c;
+  char *w = word;
+  while (isspace(c = getch()))
+    ;
+  if (c != EOF)
+    *w++ = c;
+  if (!isalpha(c)) {
+    *w = '\0';
+    return c;
+  }
+  for ( ; --lim > 0; w++) {
+    if (!isalnum(*w = getch())) {
+      ungetch(*w);
+      break;
+    }
+  }
+  *w = '\0';
+  return word[0];
+}
+
 void testGetchAndUngetch() {
   assert(buffp == 0); 
   ungetch('a');
   assert(buffp == 1); 
   getch();
   assert(buffp == 0); 
+  printf("Getch and ungetch tested.\n");
 }
 
 void testUngets() {
@@ -49,11 +81,66 @@ void testUngets() {
   assert(buffp == 0); 
   ungetch(EOF);
   assert(getch() == EOF);
+  printf("Ungets tested.\n");
 }
 
+void testGetWord() {
+  printf("Enter the following string: "
+         "ab12 AB12 12ab 12AB test 123 455abc_* 1jason2\n");
+  char c, word[MAX_LINE_LEN];
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == 'a');
+  assert(strcmp(word, "ab12") == 0);
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == 'A');
+  assert(strcmp(word, "AB12") == 0);
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '1');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '2');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == 'a');
+  assert(strcmp(word, "ab") == 0);
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '1');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '2');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == 'A');
+  assert(strcmp(word, "AB") == 0);
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == 't');
+  assert(strcmp(word, "test") == 0);
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '1');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '2');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '3');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '4');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '5');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '5');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == 'a');
+  assert(strcmp(word, "abc") == 0);
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '_');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '*');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == '1');
+  c = getword(word, MAX_LINE_LEN);
+  assert(c == 'j');
+  assert(strcmp(word, "jason2") == 0);
+  printf("getWord tested.\n");
+}
 
 int main() {
   testGetchAndUngetch();
   testUngets();
+  testGetWord();
   return 0;
 }
