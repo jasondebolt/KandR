9751ae4a99456e16685e979264cc1a4717f93e11
Jason Debolt <jbenjamin@jbenjamin-macbookpro2.roam.corp.google.com>
2015-04-03 14:49:33 -0700

    Write a program to count occurrences of c keywords (version 2)

--- count_c_keyword_occurences.c
+++ count_c_keyword_occurences.c
@@ -1,169 +1,159 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <assert.h>
 #include <string.h>
-#define MAX_LINE_LEN 14
-#define MAX_LINES 20
-#define MAX_BUFF_LEN 1024
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_BUFF_LEN 100
+#define MAX_WORD_LEN 100
+#define FILE_TERMINATE EOF
 
 char buffer[MAX_BUFF_LEN];
-size_t buffp = 0;
+int buffp = 0;
 
+
+struct key {
+  char *name;
+  int count;
+} keys[] = {
+  "abs", 0,
+  "break", 0,
+  "case", 0,
+  "do", 0,
+  "if", 0
+};
 
 char getch() {
-        return (buffp > 0) ? buffer[--buffp] : getchar();
+  return (buffp > 0) ? buffer[--buffp] : getchar();
 }
 
 void ungetch(char c) {
-        if (buffp < MAX_BUFF_LEN) {
-                buffer[buffp++] = c;
-        } else {
-                fprintf(stderr, "ERROR: buffer is full\n");
-        }
+  if (buffp < MAX_BUFF_LEN) {
+    buffer[buffp++] = c;
+  } else {
+    fprintf(stderr, "no more buffer space.\n");
+  }
 }
 
-struct key {
-        char *name;
-        int count;
-} keys[] = {
-        "abs", 0,
-        "break", 0,
-        "case", 0,
-        "do", 0,
-        "if", 0
-};
-
-
-// Gets the next token from an input stream.
-int getToken(char *s, int max_len) {
-  char c;
+int getWord(char *s, int max_len) {
+  char c, d;
   int len = 0;
-  while (isspace(c = getchar()))
+  while (isspace(c = getch()))
     ;
   *s++ = c;
-  len++;
-  while (!isspace(c = getchar()) && c != EOF) {
-    *s++ = c;
-    len++;
+  ++len;
+  if (c == FILE_TERMINATE) {
+    *s = '\0';
+    return -1;
   }
-  if (c == EOF)
-    return EOF;
+  while (!isspace(c = getch()) && --max_len > 0 && c != FILE_TERMINATE) {
+    *s++ = c;
+    ++len;
+  }
   *s = '\0';
+  if (c == FILE_TERMINATE)
+    ungetch(c);
   return len;
 }
 
-
-int binarySearch(char **list, char *p, int len) {
-        int low = 0;
-        int high = len - 1;
-        int mid, diff;
-        while (low <= high) {
-                mid = (low + high) / 2;
-                if ((diff = strcmp(p, list[mid])) < 0) {
-                        high = mid - 1;
-                } else if (diff > 0) {
-                        low = mid + 1;
-                } else {
-                        return mid;
-                }
-        }
-        return -1;
-}
-
-int binarySearch2(struct key keys[], char *p, int len) {
-        int low = 0;
-        int high = len - 1;
-        int mid, diff;
-        while (low <= high) {
-                mid = (low + high) / 2;
-                if ((diff = strcmp(p, keys[mid].name)) < 0) {
-                        high = mid - 1;
-                } else if (diff > 0) {
-                        low = mid + 1;
-                } else {
-                        return mid;
-                }
-        }
-        return -1;
+struct key *binarySearch(char *name, struct key *keys, int num_keys) {
+  struct key *low = keys;   // same as &keys[0]
+  struct key *hi = keys + num_keys - 1;
+  int diff;
+  while (low <= hi) {
+    struct key *mid = low + (hi - low) / 2;
+    if ((diff = strcmp(name, mid->name)) < 0) {
+      hi = mid - 1;
+    } else if (diff > 0) {
+      low = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return NULL;
 }
 
 
-void testKeys() {
-        assert(strcmp(keys[0].name, "abs") == 0);
-        assert(keys[3].count == 0);
-        printf("keys tested.\n");
-}
-
-
-void testGetToken() {
-        char s[MAX_LINE_LEN];
-        while (getToken(s, MAX_LINE_LEN) >= 0) {
-                printf("%s\n", s);
-        }
-        printf("getToken tested.\n");
-}
-
-
-void testBinarySearch() {
-        char *s[10] = {"apple", "berry", "cherry", "lemon", "orange"};
-        assert(binarySearch(s, "apple", 5) == 0);
-        assert(binarySearch(s, "berry", 5) == 1);
-        assert(binarySearch(s, "cherry", 5) == 2);
-        assert(binarySearch(s, "lemon", 5) == 3);
-        assert(binarySearch(s, "orange", 5) == 4);
-        assert(binarySearch(s, "not found", 5) == -1);
-        printf("binarySearch tested.\n");
-}
-
-void testBinarySearch2() {
-        struct key keys[5] = {
-                "abs", 0,
-                "break", 0,
-                "case", 0,
-                "do", 0,
-                "if", 0
-        };
-        assert(binarySearch2(keys, "abs", 5) == 0);
-        assert(binarySearch2(keys, "break", 5) == 1);
-        assert(binarySearch2(keys, "case", 5) == 2);
-        assert(binarySearch2(keys, "do", 5) == 3);
-        assert(binarySearch2(keys, "if", 5) == 4);
-        assert(binarySearch2(keys, "not found", 5) == -1);
-        printf("binarySearch2 tested\n");
-}
-
-void testLoadKeys() {
-        char s[MAX_LINE_LEN];
-        int index;
-        int num_keys = sizeof(keys) / sizeof(keys[0]);
-        printf("Enter some c keywords...\n");
-        while (getToken(s, MAX_LINE_LEN) >= 0) {
-                index = binarySearch2(keys, s, num_keys);
-                keys[index].count++;
-        }
-        int i;
-        for (i = 0; i < num_keys; ++i) {
-                printf("%s, count: %d\n", keys[i].name, keys[i].count);
-        }
-        printf("loadKeys tested.\n");
-}
-
-void testGetchAndUngetch() {
+void testGetch() {
   assert(buffp == 0);
   ungetch('a');
   assert(buffp == 1);
   assert(getch() == 'a');
   assert(buffp == 0);
-  printf("testGetchAndUngetch tested.\n");
+  printf("getch tested.\n");
+}
+
+void testGetWord() {
+  printf("enter jason\n");
+  char s[MAX_WORD_LEN];
+  int len;
+  len = getWord(s, MAX_WORD_LEN);
+  assert(len == 5);
+  assert(strcmp(s, "jason") == 0);
+  printf("name: %s\n", s);
+  printf("enter jason followed by a period.\n");
+  char p[MAX_WORD_LEN];
+  len = getWord(p, MAX_WORD_LEN);
+  assert(len == -1);
+  printf("name: %s\n", p);
+  assert(strcmp(s, "jason") == 0);
+  printf("getWord tested.\n");
+}
+
+void testGetWords() {
+  char s[MAX_WORD_LEN];
+  int len;
+  while((len = getWord(s, MAX_WORD_LEN)) >= 0) {
+    printf("%s\n", s);
+  }
+  printf("getWords tested.\n");
+}
+
+void testKeys() {
+  assert(sizeof(keys) / sizeof(keys[0]) == 5);
+  assert(keys == &keys[0]);
+  assert(keys + 1 == &keys[1]);
+  assert(keys + 4 == &keys[4]);
+  assert(strcmp((keys + 3)->name, "do") == 0);
+  printf("keys tested.\n");
 }
 
 
+void testBinarySearch() {
+  int len = sizeof(keys) / sizeof(keys[0]);
+  assert(binarySearch("abs", keys, len) == &keys[0]);
+  assert(binarySearch("break", keys, len) == &keys[1]);
+  assert(binarySearch("case", keys, len) == &keys[2]);
+  assert(binarySearch("do", keys, len) == &keys[3]);
+  assert(binarySearch("if", keys, len) == &keys[4]);
+  assert(binarySearch("not found", keys, len) == NULL);
+  printf("binarySearch tested.\n");
+}
+
+void testLoadKeys() {
+  char s[MAX_WORD_LEN];
+  int keys_len = sizeof(keys) / sizeof(keys[0]);
+  int len;
+  struct key *word;
+  while((len = getWord(s, MAX_WORD_LEN)) >= 0) {
+    word = binarySearch(s, keys, keys_len);
+    if (word != NULL)
+      word->count++;
+  }
+  struct key *k = keys;
+  for (k = keys; k < keys + keys_len; k++) {
+    if (k->count > 0)
+      printf("%s, count: %d\n", k->name, k->count);
+  }
+
+}
+
 int main() {
-        testKeys();
-        testGetchAndUngetch();
-        //testGetToken();
-        testBinarySearch();
-        testBinarySearch2();
-        testLoadKeys();
-        return 0;
+  //testGetch();
+  //testGetWord();
+  //testGetWords();
+  //testKeys();
+  //testBinarySearch();
+  testLoadKeys();
+  return 0;
 }
